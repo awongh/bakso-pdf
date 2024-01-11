31fc2946e7313f94b3d5838ccfcb99f16725fe11
awongh <akira@awongh.com>
2024-01-10 23:22:50 -0800

    add comment with reference link to puppeteer error

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,8 @@ const {join} = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
+  // render.com needs to point to a different dir to install browser
+  // https://github.com/puppeteer/puppeteer/issues/9694
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, 'node_modules', '.cache', 'puppeteer'),
 };
